29c1c27b87a6e75bd367fcb48ea0a6ab1d2adcb7
lloydrichards <lloyd.d.richards@gmail.com>
2024-03-09 20:36:13 +0100

    feat(progress): :sparkles: learned to make for loops

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ Space to play around with Go, learn the basics, and document my progress.
 
 1. [x] **Hello, World!** -- (2024-03-09) -- Setup Go using `go mod init` and
        created a simple "Hello, World!" program in `main.go`.
-2. [x] **Variables** -- (2024-03-09) -- Learned about variables and how to use
-       them in Go.
+1. [x] **Variables & Data Structures** -- (2024-03-09) -- Learned about
+       variables and how to use them in Go.
+1. [x] **Loops** -- (2024-03-09) -- Learned how to create loops using the `for`
+       keyword.
--- main.go
+++ main.go
@@ -3,30 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	// Same as `var x int = 10`
-	x := 10
-	y := 20
-	z := x + y
-	fmt.Println(z)
+	// For loop
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
 
-	// Arrays
-	// create and array by `var a [5]int` <- 5 is the length of the array, can't be changed
-	a := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(a)
+	// While loop
+	j := 0
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
 
-	// Slices
-	// create a slice by `var a []int` <- no length, can be changed
-	b := []int{1, 2, 3, 4, 5}
-	// append to a slice
-	b = append(b, 6, 7, 8, 9, 10)
-	// remove from a slice
-	b = append(b[:2], b[4:]...)
-	fmt.Println(b)
-
-	// Maps
-	// create a map by `var a map[string]int` <- string is the key type, int is the value type
-	c := map[string]int{"foo": 1, "bar": 2}
-	// add to a map
-	c["baz"] = 3
-	fmt.Println(c)
+	// Infinite loop
+	k := 0
+	for {
+		fmt.Println(k)
+		k++
+		if k == 5 {
+			break
+		}
+	}
 }

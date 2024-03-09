d68e429978786cb21aba1a70add21172c1b46ecf
lloydrichards <lloyd.d.richards@gmail.com>
2024-03-09 20:32:34 +0100

    feat(progress): :sparkles: learn about data types and structures

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Space to play around with Go, learn the basics, and document my progress.
 
 1. [x] **Hello, World!** -- (2024-03-09) -- Setup Go using `go mod init` and
        created a simple "Hello, World!" program in `main.go`.
+2. [x] **Variables** -- (2024-03-09) -- Learned about variables and how to use
+       them in Go.
--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Same as `var x int = 10`
+	x := 10
+	y := 20
+	z := x + y
+	fmt.Println(z)
+
+	// Arrays
+	// create and array by `var a [5]int` <- 5 is the length of the array, can't be changed
+	a := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(a)
+
+	// Slices
+	// create a slice by `var a []int` <- no length, can be changed
+	b := []int{1, 2, 3, 4, 5}
+	// append to a slice
+	b = append(b, 6, 7, 8, 9, 10)
+	// remove from a slice
+	b = append(b[:2], b[4:]...)
+	fmt.Println(b)
+
+	// Maps
+	// create a map by `var a map[string]int` <- string is the key type, int is the value type
+	c := map[string]int{"foo": 1, "bar": 2}
+	// add to a map
+	c["baz"] = 3
+	fmt.Println(c)
 }

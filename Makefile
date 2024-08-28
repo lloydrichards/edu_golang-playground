18dcb2b9ada96f7c64c83a09311d706b8f4cae02
lloydrichards <lloyd.d.richards@gmail.com>
2024-08-28 11:26:57 +0200

    feat(calculator-api): :sparkles: add basic http server

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	go build -o build/playground main.go
+	go build -o build/calculator-api calculator-api/main.go
+	go build -o build/todo-cli todo-cli/main.go
+dev: build
+	go run .
+dev-api: build
+	go run ./calculator-api/main.go
--- calculator-api/main.go
+++ calculator-api/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "hello world"}`))
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Println("Starting the server on http://localhost:8080")
+	server.ListenAndServe()
 }
